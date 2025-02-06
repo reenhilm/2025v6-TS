fd02677fb40a5c5cf06079907beac976b441d6c4
Christian Rönnholm <christian@kajal.se>
2025-02-06 11:12:14 +0100

    first commit, completed part 1

---
+++ src/script.ts
@@ -0,0 +1,11 @@
+//1a done (Skapa ett nytt npm-projekt)
+
+//1b done (Skapa en config-fil för TypeScript)
+
+//1c
+let x: number = 42;
+
+//1d
+const myString: string = 'foo';
+const myNumber: number = 123;
+const myBool: boolean = true;
